689ef77f65f12816f3edc1abe23a8438d5d9fe86
郭斌勇 <guobinyong@qq.com>
2020-06-30 19:25:44 +0800

    增加wrap方法

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ type AfterFun = (rawReturn:any,...rawArguments:any[])=>any
 interface Function {
     before(fun:BeforeFun):this;
     after(fun:AfterFun):this;
+    wrap(before?:BeforeFun|null|undefined,after?:AfterFun|null|undefined):this;
 }
 
 
@@ -18,7 +19,7 @@ Function.prototype.before = function (fun) {
 
         // @ts-ignore
         return rawFun.apply(this,argArr);
-    }
+    };
 }
 
 
@@ -32,5 +33,40 @@ Function.prototype.after = function (fun) {
         var rawReturn = rawFun.apply(this,arguments);
         // @ts-ignore
         return  fun.call(this,rawReturn,...arguments);
-    }
+    };
 }
+
+
+
+
+Function.prototype.wrap = function (before,after) {
+    var rawFun = this;
+
+    if (before && after){
+        return function () {
+            // @ts-ignore
+            var argArr = before.apply(this,arguments);
+            if (argArr == null){
+                argArr = arguments;
+            }
+
+            // @ts-ignore
+            var rawReturn = rawFun.apply(this,argArr);
+
+            // @ts-ignore
+            return  fun.call(this,rawReturn,...arguments);
+        };
+    }
+
+
+    if (before){
+        return rawFun.before(before);
+    }
+
+    if (after){
+        return rawFun.after(after);
+    }
+
+    return rawFun;
+}
+
