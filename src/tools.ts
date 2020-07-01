05be0d8a324e9c4df54a81915eaedd5955538ae7
郭斌勇 <guobinyong@qq.com>
2020-07-01 11:40:33 +0800

    增加注释

--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,8 @@
+/**
+ * 为指定的函数 fun 创建对应的 单例函数
+ * @param fun : Function  普通函数 或者 构造函数
+ * @return Function  增加了单例机制的函数；该函数的使用方式 和 原函数 fun 一样，只是无论多少次调用该函数，该函数都会返回原函数 fun 的同一实例
+ */
 export function createSingleton<Fun extends Function>(fun:Fun):Fun {
     var singleInst:any;
 
